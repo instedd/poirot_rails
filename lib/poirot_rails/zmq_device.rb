c23c1c8c0b23c92328c3329b60abe2832a0fa2a2
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-11-17 16:53:19 -0300

    Hook with Spring and Resque after_fork callbacks to reconnect with Poirot server

--- lib/poirot_rails/zmq_device.rb
+++ lib/poirot_rails/zmq_device.rb
@@ -3,14 +3,26 @@ require 'ffi-rzmq'
 module PoirotRails
   class ZMQDevice
     def initialize
+      connect
+
+      if defined?(::Resque)
+        ::Resque.after_fork { connect }
+      end
+
+      if defined?(::Spring)
+        ::Spring.after_fork { connect }
+      end
+
+      at_exit { close }
+    end
+
+    def connect
       @zmq_context = ZMQ::Context.new
       @zmq_socket = @zmq_context.socket(ZMQ::PUB)
       @zmq_socket.setsockopt ZMQ::RCVHWM, 50
       @zmq_socket.setsockopt ZMQ::SNDHWM, 50
       @zmq_socket.setsockopt ZMQ::LINGER, 1000
       @zmq_socket.connect("tcp://#{PoirotRails.server}")
-
-      at_exit { close }
     end
 
     def write(data)
