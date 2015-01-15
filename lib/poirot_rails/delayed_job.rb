0737151f3bef86c0a7255656bbc65a2c1a847882
Juan Wajnerman <jwajnerman@manas.com.ar>
2015-01-15 20:07:31 -0300

    Intercept delayed_job enqueue and job running to generate linked activities

--- lib/poirot_rails/delayed_job.rb
+++ lib/poirot_rails/delayed_job.rb
@@ -1,18 +1,59 @@
-module PoirotRails
-  module DelayedJob
-    def before(job)
-      desc = description rescue "DelayedJob ##{job.id}"
-      activity = Activity.new(Guid.new.to_s, desc, { delayed_job_id: job.id })
-      Activity.push activity
-    end
+if defined?(Delayed::Job)
+  module Delayed
+    module Backend
+      module Base
+        module ClassMethods
+          def enqueue_with_poirot(*args)
+            if activity_id = PoirotRails::Activity.current.id
+              link_id = Guid.new.to_s
+              args[0].instance_variable_set :@_poirot_activity_id, activity_id
+              args[0].instance_variable_set :@_poirot_link_id, link_id
+              PoirotRails::Activity.current.logentry(:info, "Enqueue '#{args[0].class}'", ["delayed_job"], link_id: link_id)
+            end
+            enqueue_without_poirot(*args)
+          end
 
-    def after(job)
-      Activity.pop
-    end
+          alias_method_chain :enqueue, :poirot
+        end
 
-    def error(job, exception)
-      PoirotRails.logentry :error, "#{exception.class.name}: #{exception.message}"
+        def invoke_job_with_poirot
+          if activity_id = payload_object.instance_variable_get(:@_poirot_activity_id)
+            PoirotRails::Activity.resume(activity_id) do
+              link_id = payload_object.instance_variable_get(:@_poirot_link_id)
+              invoke_job_in_new_activity(link_id)
+            end
+          else
+            invoke_job_in_new_activity
+          end
+        end
+
+        alias_method_chain :invoke_job, :poirot
+
+        def invoke_job_in_new_activity(link_id = nil)
+          metadata = { async: true, attempt: attempts + 1 }
+          metadata[:link_id] = link_id if link_id
+
+          (payload_object.instance_variables - [:@_poirot_activity_id, :@_poirot_link_id]).each do |ivar|
+            metadata[ivar.to_s[1..-1]] = payload_object.instance_variable_get(ivar)
+          end
+
+          if respond_to?(:id)
+            description = "#{payload_object.class} (#{id})"
+            metadata[:delayed_job_id] = id
+          else
+            description = payload_object.class.to_s
+          end
+
+          PoirotRails::Activity.start(description, metadata) do
+            begin
+              invoke_job_without_poirot
+            rescue Exception => ex
+              PoirotRails::Activity.current.logentry(:error, "Unhandled exception: #{ex}")
+              raise
+            end
+          end
+        end
+      end
     end
   end
 end
-
